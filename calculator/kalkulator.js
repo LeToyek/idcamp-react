2c072ec49444b66922dfa08e8cda9b18fea3c8b9
LeToyek <maulanatoyek@gmail.com>
2022-05-30 23:52:10 +0700

    baru mulai

--- calculator/kalkulator.js
+++ calculator/kalkulator.js
@@ -1,5 +1,46 @@
-console.log("this is javascript command")
-let myArray = ["Harry", "Ron", "Hermione", "Tom"];
-for (const arr of myArray) {
-    console.log(arr)
+console.log("bisa ga ges")
+const calculator = {
+  displayNumber: "0",
+  operator: null,
+  firstNumber: null,
+  waitingForSecondNumber: false,
+};
+function coba(){
+  
+}
+function updateDisplay() {
+  document.querySelector('#displayNumber').innerHTML = calculator.displayNumber
+  
+}
+
+function clearCalculator() {
+  calculator.displayNumber = "0";
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+  if (calculator.displayNumber === '0') {
+    calculator.displayNumber = digit;
+  }else{
+    calculator.displayNumber += digit;
+  }
+}
+const buttons = document.querySelectorAll(".button")
+
+for (let button of buttons) {
+  button.addEventListener('click',function (event) {
+    
+    const target = event.target
+    
+    if(target.class) {
+      clearCalculator();
+      updateDisplay();
+      return;
+  }
+
+    inputDigit(target.textContent)
+    updateDisplay()
+  })
 }
