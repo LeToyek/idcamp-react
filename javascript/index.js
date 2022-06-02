de37df49aec3577cdadf521961a3021d1692cf22
LeToyek <maulanatoyek@gmail.com>
2022-06-02 22:41:03 +0700

    kuis

--- javascript/index.js
+++ javascript/index.js
@@ -6,11 +6,51 @@ const user2 ={
     name: "p",
     age:3
 }
+const {name ,age} = user2
+console.log(name)
 arr = [
     user,
     user,
     user2
 ]
-for (const users of arr) {
-    console.log(users.age)
+const greetings = function (name,lang){
+    if (lang === 'jawa') {
+        return 'Sugeng rawuh ' + name
+    }else if (lang === 'sunda') {
+        return 'Sampurasun '+ name
+    }else{
+        return 'tang ina nak sasalita kama isa'
+    }
 }
+
+console.log(greetings('toyek','sunda'))
+
+const kerja = (pekerjaan) => console.log('sedang ' + pekerjaan)
+kerja('nguli')
+
+const belajar = (nama, subject) =>{
+    return nama + ' sedang belajar ' + subject
+}
+
+console.log(belajar('toyek','ngoding'))
+console.log(...arr.map(r=>r.name))
+
+const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
+
+const newArray = names.map((e)=>`${e}!`)
+console.log(newArray)
+
+const pureFunction = (person,age) =>{
+    return {...person,age}
+}
+const person = {
+    nomme: 'toyek',
+    age: 19
+}
+
+const newPerson = pureFunction(person,13)
+const newPerson2 = pureFunction(person,14)
+
+console.log(person)
+console.log(newPerson)
+console.log(newPerson2)
